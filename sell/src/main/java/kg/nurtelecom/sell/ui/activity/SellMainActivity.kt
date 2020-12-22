69bcf0c212e594e8eb6b41db324b890fa5f1329a
Zaiyr Sharsheyev <zaiyrsharsheyev@MacBook-Pro-Zaiyr.local>
2020-12-23 02:30:40 +0600

    added side menu to SellMainActivity, closed task #10.04

--- sell/src/main/java/kg/nurtelecom/sell/ui/activity/SellMainActivity.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/activity/SellMainActivity.kt
@@ -1,9 +1,16 @@
 package kg.nurtelecom.sell.ui.activity
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import kg.nurtelecom.core.activity.CoreActivity
+import kg.nurtelecom.core.extension.replaceFragment
+import kg.nurtelecom.core.extension.setToolbarTitle
 import kg.nurtelecom.sell.R
 import kg.nurtelecom.sell.databinding.ActivitySellMainBinding
+import kg.nurtelecom.sell.databinding.SideMenuSellMainBinding
 import kg.nurtelecom.sell.ui.fragment.sell.SellFragment
 
 class SellMainActivity :
@@ -11,15 +18,42 @@ class SellMainActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        navigateToSellFragment()
+    }
+
+    override fun setupViews() {
+        super.setupViews()
+        setSupportActionBar(vb.tbSellMain)
+        setupNavDrawer()
+//        replaceFragment(R.id.container, SellFragment.newInstance())
+    }
+
+    private fun setupNavDrawer() {
+        val actionBarToggle: ActionBarDrawerToggle = ActionBarDrawerToggle(this,vb.drawerLayout, vb.tbSellMain,R.string.content_text, R.string.content_text )
+        actionBarToggle.drawerArrowDrawable.color = resources.getColor(R.color.colorWhite)
+        vb.drawerLayout.addDrawerListener(actionBarToggle)
+        setupDrawerListener()
+        actionBarToggle.syncState()
+    }
+
+    private fun setupDrawerListener() {
+        vb.drawerLayout.addDrawerListener(drawerListener())
+        val view = vb.navView.getHeaderView(0)
+        val v = SideMenuSellMainBinding.bind(view)
+    }
+
+    private fun drawerListener(): DrawerLayout.DrawerListener {
+        return object : DrawerLayout.DrawerListener {
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+            override fun onDrawerStateChanged(newState: Int) {}
+            override fun onDrawerClosed(drawerView: View) {}
+
+            override fun onDrawerOpened(drawerView: View) {
+                setToolbarTitle(resources.getString(R.string.text_menu))
+            }
+        }
     }
 
     override fun getBinding(): ActivitySellMainBinding =
         ActivitySellMainBinding.inflate(layoutInflater)
-
-    private fun navigateToSellFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, SellFragment.newInstance())
-            .commit()
-    }
 }
