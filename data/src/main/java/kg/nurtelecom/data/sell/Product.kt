e0e624f56fef0997f97b5db1c5e341bf1f870d2a
Бектурсун Курманбеков <kurmanbekov46@gmail.com>
2020-12-23 13:47:20 +0600

    fix bug when displaying discount and allowance, close task #12.09

--- data/src/main/java/kg/nurtelecom/data/sell/Product.kt
+++ data/src/main/java/kg/nurtelecom/data/sell/Product.kt
@@ -3,19 +3,9 @@ package kg.nurtelecom.data.sell
 import java.math.BigDecimal
 
 data class Product(
-    val price: BigDecimal,
+    val price: BigDecimal? = null,
     val count: BigDecimal,
-    val discount: BigDecimal,
-    val allowance: BigDecimal
-) {
-    val totalPrice: BigDecimal
-    get() {
-        // We are subtracting the discount from the main amount
-        // and adding an allowance to the main amount to get the total price
-        val totalPrice = price.multiply(count)
-        val hundred = BigDecimal("100.0")
-        val discount = totalPrice.multiply(this.discount).divide(hundred)
-        val allowance = totalPrice.multiply(this.allowance).divide(hundred)
-        return totalPrice.subtract(discount).add(allowance)
-    }
-}
+    val totalPrice: BigDecimal,
+    val discount: BigDecimal? = null,
+    val allowance: BigDecimal? = null
+)
