3dd46d363826acad75950bf7b112cd3c89133698
Бекзадбек Насирахунов <n.bekzad@gmail.com>
2020-12-25 00:13:54 +0600

    Added pay by card and cash fragments

--- sell/src/main/java/kg/nurtelecom/sell/ui/fragment/payment_method/PaymentMethodFragment.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/fragment/payment_method/PaymentMethodFragment.kt
@@ -1,8 +1,13 @@
 package kg.nurtelecom.sell.ui.fragment.payment_method
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
+import kg.nurtelecom.sell.R
 import kg.nurtelecom.sell.databinding.FragmentPaymentMethodBinding
 import kg.nurtelecom.sell.ui.activity.SellMainViewModel
 import kg.nurtelecom.sell.ui.core.CoreFragment
@@ -13,7 +18,40 @@ class PaymentMethodFragment : CoreFragment<FragmentPaymentMethodBinding>() {
     override val vm: SellMainViewModel by activityViewModels()
 
     override fun setupViews() {
-        super.setupViews()
+        navigate()
+    }
+
+    private fun navigate() {
+        vb.btnCashPayment.setOnClickListener {
+            val activity = activity as AppCompatActivity
+            activity
+                .supportFragmentManager
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.container, PaymentByCashFragment())
+                .commit()
+        }
+
+        vb.btnCardPayment.setOnClickListener {
+            val activity = activity as AppCompatActivity
+            activity
+                .supportFragmentManager
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.container, PaymentByCardFragment.newInstance())
+                .commit()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToLiveData()
+    }
+
+    private fun subscribeToLiveData() {
+        vm.calculateTaxSum().observe(viewLifecycleOwner) {sum ->
+            vb.twPaymentAmount.text = sum.toString()
+        }
     }
 
     override fun createViewBinding(
