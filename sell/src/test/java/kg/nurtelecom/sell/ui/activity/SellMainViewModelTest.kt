6b6bb7a5626ab22fb8935b3f01aed6a544cfbf0e
Бектурсун Курманбеков <kurmanbekov46@gmail.com>
2020-12-24 11:09:31 +0600

    add test

---
+++ sell/src/test/java/kg/nurtelecom/sell/ui/activity/SellMainViewModelTest.kt
@@ -0,0 +1,25 @@
+package kg.nurtelecom.sell.ui.activity
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SellMainViewModelTest {
+
+    @Test
+    fun getProductList() {
+
+    }
+
+    @Test
+    fun getTaxSum() {
+    }
+
+    @Test
+    fun addNewProduct() {
+    }
+
+    @Test
+    fun removeProductFromList() {
+    }
+}
