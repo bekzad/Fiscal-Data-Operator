84a27e0d8a599d146b1561beb537d879e55e776d
Бектурсун Курманбеков <kurmanbekov46@gmail.com>
2020-12-24 23:59:17 +0600

    add start activity extension, interface click listener, close #12.11, #12.10

---
+++ sell/src/main/java/kg/nurtelecom/sell/core/ItemClickListener.kt
@@ -0,0 +1,5 @@
+package kg.nurtelecom.sell.core
+
+interface ItemClickListener {
+    fun removeItem(position: Int)
+}
--- sell/src/main/java/kg/nurtelecom/sell/ui/fragment/add_product/AddProductFragment.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/fragment/add_product/AddProductFragment.kt
@@ -2,36 +2,27 @@ package kg.nurtelecom.sell.ui.fragment.add_product
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
-import kg.nurtelecom.sell.R
 import kg.nurtelecom.sell.core.CoreFragment
 import kg.nurtelecom.sell.databinding.AddProductFragmentBinding
 import kg.nurtelecom.sell.ui.activity.SellMainViewModel
 import kg.nurtelecom.sell.ui.core.CoreFragment
 import kg.nurtelecom.sell.ui.fragment.price_output.PriceOutputFragment
+import kg.nurtelecom.sell.utils.addFragment
+import kg.nurtelecom.sell.utils.getFragmentActivity
 
 class AddProductFragment : CoreFragment<AddProductFragmentBinding>() {
 
     override val vm: SellMainViewModel by activityViewModels()
 
     override fun setupViews() {
-        vb.productNotFromListButton.setOnClickListener { navigate() }
+        vb.productNotFromListButton.setOnClickListener {
+            getFragmentActivity().addFragment(PriceOutputFragment.newInstance())
+        }
     }
 
-    private fun navigate() {
-        val activity = activity as AppCompatActivity
-        activity.supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.sell_container, PriceOutputFragment.newInstance())
-            .addToBackStack(null)
-            .commit()
-    }
-
-    override fun createViewBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): AddProductFragmentBinding = AddProductFragmentBinding.inflate(inflater, container, false)
+    override fun createViewBinding(inflater: LayoutInflater, container: ViewGroup?) =
+        AddProductFragmentBinding.inflate(inflater, container, false)
 
     companion object {
         fun newInstance() = AddProductFragment()
