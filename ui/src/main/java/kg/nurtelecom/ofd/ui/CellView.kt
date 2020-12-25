4c6172cca3be2b259aafcca685ede563f24b5701
Эрлан Алмаз <erlan.almaz10@gmail.com>
2020-12-25 14:17:40 +0600

    Change CellView

--- ui/src/main/java/kg/nurtelecom/ofd/ui/CellView.kt
+++ ui/src/main/java/kg/nurtelecom/ofd/ui/CellView.kt
@@ -2,10 +2,7 @@ package kg.nurtelecom.ofd.ui
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import kg.nurtelecom.ui.R
 import kg.nurtelecom.ui.databinding.CellViewBinding
@@ -14,66 +11,58 @@ class CellView(context: Context, attr: AttributeSet) : ConstraintLayout(context,
 
     private val vb = CellViewBinding.inflate(LayoutInflater.from(context), this, true)
 
-    private val position: Int
-    private var title: String
-    private var subTitle: String
-    private val showArrowIcon: Boolean
-    private val showSubTitle: Boolean
-
     init {
         context.theme.obtainStyledAttributes(
             attr,
             R.styleable.custom_view,
-            0, 0).apply {
+            0, 0
+        ).apply {
             try {
-                position = getInteger(R.styleable.custom_view_position, 1)
-                title = getString(R.styleable.custom_view_title).toString()
-                subTitle = getString(R.styleable.custom_view_subTitle).toString()
-                showArrowIcon = getBoolean(R.styleable.custom_view_showArrowIcon, true)
-                showSubTitle = getBoolean(R.styleable.custom_view_showSubTitle, true)
-                setupViews()
+                vb.title.text = getString(R.styleable.custom_view_title).toString()
+                vb.subTitle.text = getString(R.styleable.custom_view_subTitle).toString()
+                setIconIsVisible(getBoolean(R.styleable.custom_view_showArrowIcon, true))
+                setSubTitleIsVisible(getBoolean(R.styleable.custom_view_showSubTitle, true))
             } finally {
                 recycle()
             }
         }
     }
 
-    private fun setupViews() {
-        vb.title.text = title
-        vb.subTitle.text = subTitle
-        if (!showArrowIcon) {
-            vb.icon.visibility = View.GONE
-        }
-        if (!showSubTitle) {
-            vb.subTitle.visibility = View.GONE
-        }
-        when (position) {
-            0 -> vb.root.background = context.resources.getDrawable(R.drawable.rounded_top_bg)
-            1 -> vb.root.background = context.resources.getDrawable(R.drawable.rounded_mid_bg)
-            2 -> vb.root.background = context.resources.getDrawable(R.drawable.rounded_bottom)
-            3 -> vb.root.background = context.resources.getDrawable(R.drawable.rounded_single_bg)
-        }
-    }
-
     override fun setOnClickListener(l: OnClickListener?) {
-        super.setOnClickListener(l)
+        //super.setOnClickListener(l)
         vb.rootLayout.setOnClickListener(l)
     }
 
     fun setTitle(value: String) {
-        title = value
-        vb.title.text = title
+        vb.title.text = value
     }
 
     fun setSubtitle(value: String) {
-        subTitle = value
-        vb.subTitle.text = subTitle
+        vb.subTitle.text = value
     }
 
-    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-        Log.i("ERLAN", "onInterceptorTouchEvent")
-        return true
+    fun setSubTitleIsVisible(value: Boolean) {
+        if (value)
+            vb.subTitle.visibility = VISIBLE
+        else
+            vb.subTitle.visibility = GONE
     }
+
+    fun setIconIsVisible(value: Boolean) {
+        if (value)
+            vb.cellViewIcon.visibility = VISIBLE
+        else
+            vb.cellViewIcon.visibility = GONE
+    }
+
+    fun setCellIcon(resId: Int) {
+        vb.cellViewIcon.setImageResource(resId)
+    }
+
+//    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+//        Log.i("ERLAN", "onInterceptorTouchEvent")
+//        return true
+//    }
 
 
 }
