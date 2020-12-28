8536417e0ab94d9123c4254f5db6829b7e54123b
Эрлан Алмаз <erlan.almaz10@gmail.com>
2020-12-28 12:20:56 +0600

    Remove setSubTitleVisibility fun rename function's attributes remove mxl subTitleVisibility attribute

--- ui/src/main/java/kg/nurtelecom/ofd/ui/CellView.kt
+++ ui/src/main/java/kg/nurtelecom/ofd/ui/CellView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import kg.nurtelecom.core.extension.visible
 import kg.nurtelecom.ui.R
 import kg.nurtelecom.ui.databinding.CellViewBinding
 
@@ -18,10 +19,9 @@ class CellView(context: Context, attr: AttributeSet) : ConstraintLayout(context,
             0, 0
         ).apply {
             try {
-                vb.title.text = getString(R.styleable.custom_view_title).toString()
-                vb.subTitle.text = getString(R.styleable.custom_view_subTitle).toString()
+                setTitle(getString(R.styleable.custom_view_title).toString())
+                setSubTitle(getString(R.styleable.custom_view_subTitle))
                 setIconIsVisible(getBoolean(R.styleable.custom_view_showArrowIcon, true))
-                setSubTitleIsVisible(getBoolean(R.styleable.custom_view_showSubTitle, true))
             } finally {
                 recycle()
             }
@@ -33,26 +33,17 @@ class CellView(context: Context, attr: AttributeSet) : ConstraintLayout(context,
         vb.rootLayout.setOnClickListener(l)
     }
 
-    fun setTitle(value: String) {
-        vb.title.text = value
+    fun setTitle(titleText: String) {
+        vb.title.text = titleText
     }
 
-    fun setSubtitle(value: String) {
-        vb.subTitle.text = value
-    }
-
-    fun setSubTitleIsVisible(value: Boolean) {
-        if (value)
-            vb.subTitle.visibility = VISIBLE
-        else
-            vb.subTitle.visibility = GONE
+    fun setSubTitle(subTitleText: String?) {
+        vb.subTitle.visible(!subTitleText.isNullOrEmpty())
+        vb.subTitle.text = subTitleText
     }
 
     fun setIconIsVisible(value: Boolean) {
-        if (value)
-            vb.cellViewIcon.visibility = VISIBLE
-        else
-            vb.cellViewIcon.visibility = GONE
+        vb.cellViewIcon.visible(value)
     }
 
     fun setCellIcon(resId: Int) {
