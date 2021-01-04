07161c5dda7e9e4c303bddd91c5b802efa235e33
Бектурсун Курманбеков <kurmanbekov46@gmail.com>
2021-01-04 18:53:52 +0600

    add custom dialog view, close task #16.01

--- sell/src/main/java/kg/nurtelecom/sell/ui/custom/CustomDialog.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/custom/CustomDialog.kt
@@ -1,44 +1,52 @@
 package kg.nurtelecom.sell.ui.custom
 
 import android.content.Context
-import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import androidx.annotation.RequiresApi
-import androidx.annotation.StringRes
-import androidx.annotation.StyleRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import kg.nurtelecom.sell.R
 import kg.nurtelecom.sell.databinding.DialogViewBinding
 
-// TODO: must be change
-class CustomDialog(context: Context, attributeSet: AttributeSet? = null) : ConstraintLayout(context, attributeSet) {
+
+class CustomDialog(context: Context, attributeSet: AttributeSet? = null) :
+    ConstraintLayout(context, attributeSet) {
 
     private val binding = DialogViewBinding.inflate(LayoutInflater.from(context), this, true)
 
-    /*fun setDialogTitle(@StringRes text: Int = R.string.fiscal_regime_title) {
-        binding.regimeTv.setText(text)
+    fun setupDialog(regime: String) {
+        when (regime) {
+            FISCAL_REGIME -> setupFiscalDialog(FISCAL_REGIME)
+            NON_FISCAL_REGIME -> setupFiscalDialog(NON_FISCAL_REGIME)
+            else -> return
+        }
     }
 
-    fun setDialogContent(@StringRes text: Int = R.string.fiscal_regime_content) {
-        binding.regimeMessage.setText(text)
-    }*/
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun setTitleStyle(@StyleRes style: Int) {
-        binding.regimeTv.setTextAppearance(style)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun setContentStyle(@StyleRes style: Int) {
-        binding.regimeMessage.setTextAppearance(style)
-    }
-
-    fun setInvisible() {
+    fun hideDialog() {
         binding.regimeOkButton.setOnClickListener {
             binding.root.visibility = View.GONE
         }
     }
 
+    private fun setupFiscalDialog(regime: String) {
+        when (regime) {
+            FISCAL_REGIME -> {
+                binding.apply {
+                    regimeTv.setText(R.string.fiscal_mode_title)
+                    regimeMessage.setText(R.string.fiscal_mode_dialog_content)
+                }
+            }
+            NON_FISCAL_REGIME -> {
+                binding.apply {
+                    regimeTv.setText(R.string.non_fiscal_mode_title)
+                    regimeMessage.setText(R.string.non_fiscal_mode_dialog_content)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val FISCAL_REGIME: String = "fiscal"
+        const val NON_FISCAL_REGIME: String = "nonFiscal"
+    }
 }
