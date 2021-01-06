f29515db4332b1e9dcbb5607810bb92b1793a620
akinahikaru <bakytova120496@gmail.com>
2021-01-06 08:10:57 +0600

    created RegEx validateOfTheIdentificationNumber ()

--- user/src/main/java/kg/nurtelecom/user/ui/UserActivity.kt
+++ user/src/main/java/kg/nurtelecom/user/ui/UserActivity.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.Observer
 import kg.nurtelecom.core.activity.CoreActivity
 import kg.nurtelecom.core.fragment.CoreFragment
 import kg.nurtelecom.user.databinding.ActivityUserBinding
+import java.util.regex.Pattern
 
 class UserActivity : CoreActivity<ActivityUserBinding, UserVM>(UserVM::class) {
 
     override fun subscribeToLiveData() {
         super.subscribeToLiveData()
         observeUserData()
+        validateOfTheIdentificationNumber ()
     }
     private fun observeUserData(){
         vm.userData.observe(this, Observer {
@@ -26,13 +28,24 @@ class UserActivity : CoreActivity<ActivityUserBinding, UserVM>(UserVM::class) {
     }
     override fun getBinding() = ActivityUserBinding.inflate(layoutInflater)
 
+    private fun validateOfTheIdentificationNumber (): Boolean {
+        val ID_PATTERN =
+            Pattern.compile("^(1|2)(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])(19|20)\\d\\d\\d\\d\\d\\d\\d\$")
+        val identificationNumberInput = vb.etIdentification.text.toString().trim()
+        if (!ID_PATTERN.matcher(identificationNumberInput).matches()) {
+            vb.etIdentification.error = "The ID number is not correct"
+            return false
+        }else{
+            vb.etIdentification.error =  null
+            return true
+        }
+    }
+
     companion object {
 
         fun newInstance(): UserActivity{
             return UserActivity()
         }
-
     }
-
 }
 
