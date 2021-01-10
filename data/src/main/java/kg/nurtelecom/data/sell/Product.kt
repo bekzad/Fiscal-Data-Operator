a90a916d0079d116f14c619d9e9406730007aa5f
Бектурсун Курманбеков <kurmanbekov46@gmail.com>
2021-01-11 01:00:50 +0600

    change Product data class

--- data/src/main/java/kg/nurtelecom/data/sell/Product.kt
+++ data/src/main/java/kg/nurtelecom/data/sell/Product.kt
@@ -3,9 +3,26 @@ package kg.nurtelecom.data.sell
 import java.math.BigDecimal
 
 data class Product(
-    val price: BigDecimal? = null,
+    val price: BigDecimal,
     val count: BigDecimal,
     val totalPrice: BigDecimal,
-    val discount: BigDecimal? = null,
-    val allowance: BigDecimal? = null
-)
+    private val discount: BigDecimal,
+    private val allowance: BigDecimal
+) {
+    val verifiedDiscount = if (discount.isNotZero()) ("+ $discount% ") else ""
+    val verifiedAllowance = if (allowance.isNotZero()) ("+ $allowance%") else ""
+}
+
+fun BigDecimal.isNotZero(): Boolean {
+    return compareTo(BigDecimal.ZERO) != 0
+}
+
+fun Product.fetchExpression(): StringBuilder {
+    return StringBuilder().also {
+        it.append("$price * ")
+        it.append("${count.toInt()} ")
+        it.append(verifiedDiscount)
+        it.append(verifiedAllowance)
+        it.trimEnd()
+    }
+}
