b518f7d81ad5d5794801c1d605025d24121b500f
Zaiyr <zaiyr.00@gmail.com>
2021-01-11 22:52:20 +0600

    item decortion for checks history

--- sell/src/main/java/kg/nurtelecom/sell/ui/fragment/history/HistoryFragment.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/fragment/history/HistoryFragment.kt
@@ -3,17 +3,16 @@ package kg.nurtelecom.sell.ui.fragment.history
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import kg.nurtelecom.core.extension.formatForLocalDateTimeDefaults
+import kg.nurtelecom.core.extension.formatForDecoratorDateTimeDefaults
 import kg.nurtelecom.core.extension.setToolbarTitle
 import kg.nurtelecom.core.fragment.CoreFragment
-import kg.nurtelecom.data.history.Content
 import kg.nurtelecom.sell.R
 import kg.nurtelecom.sell.databinding.HistoryRecycleViewBinding
 import java.text.SimpleDateFormat
 
 class HistoryFragment : CoreFragment<HistoryRecycleViewBinding, HistoryViewModel>(HistoryViewModel::class) {
 
-    private var historyAdapter: HistoryAdapterFake = HistoryAdapterFake()
+    private var historyAdapter: HistoryAdapter = HistoryAdapter()
 
     override fun onResume() {
         super.onResume()
@@ -27,13 +26,8 @@ class HistoryFragment : CoreFragment<HistoryRecycleViewBinding, HistoryViewModel
 
     private fun initRecyclerView() {
         vb.historyRecyclerView.apply {
-
-//            addItemDecoration(
-//                StickyHeaderDateDecoration(historyAdapter, vb.root)
-//            )
-
-//            layoutManager = LinearLayoutManager(activity)
-//            adapter = historyAdapter
+            vb.historyRecyclerView.layoutManager = LinearLayoutManager(activity)
+            vb.historyRecyclerView.adapter = historyAdapter
         }
     }
 
@@ -45,15 +39,11 @@ class HistoryFragment : CoreFragment<HistoryRecycleViewBinding, HistoryViewModel
     private fun observeCheckHistory() {
         vm.checksHistoryData.observe(this, Observer {
             if(it != null) {
-                val items = it
-                val groupedBooks = items.groupBy { book -> SimpleDateFormat("yyyy-MM-dd'T'HH:mm:SSS").parse(book.createdAt).formatForLocalDateTimeDefaults().last().toUpperCase() }
-                historyAdapter.bookData = groupedBooks.toSortedMap()
-
+                val groupedItems = it.groupBy { book -> SimpleDateFormat("yyyy-MM-dd'T'HH:mm:SSS").parse(book.createdAt).formatForDecoratorDateTimeDefaults() }
+                historyAdapter.itemData = groupedItems.toSortedMap()
                 vb.historyRecyclerView.addItemDecoration(
                     StickyHeaderDateDecoration(historyAdapter, vb.root)
                 )
-                vb.historyRecyclerView.layoutManager = LinearLayoutManager(activity)
-                vb.historyRecyclerView.adapter = historyAdapter
             }
         })
     }
