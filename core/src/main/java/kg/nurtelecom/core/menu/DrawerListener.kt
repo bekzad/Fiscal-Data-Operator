6563d4d9ebef766192c762358ffcde6a9c1ce473
Zaiyr <zaiyr.00@gmail.com>
2021-01-13 13:54:11 +0600

    code refactoring for checks history

--- core/src/main/java/kg/nurtelecom/core/menu/DrawerListener.kt
+++ core/src/main/java/kg/nurtelecom/core/menu/DrawerListener.kt
@@ -4,19 +4,8 @@ import android.view.View
 import androidx.drawerlayout.widget.DrawerLayout
 
 abstract class DrawerListener : DrawerLayout.DrawerListener {
-    override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-
-    }
-
-    override fun onDrawerOpened(drawerView: View) {
-
-    }
-
-    override fun onDrawerClosed(drawerView: View) {
-
-    }
-
-    override fun onDrawerStateChanged(newState: Int) {
-
-    }
+    override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+    override fun onDrawerOpened(drawerView: View) {}
+    override fun onDrawerClosed(drawerView: View) {}
+    override fun onDrawerStateChanged(newState: Int) {}
 }
