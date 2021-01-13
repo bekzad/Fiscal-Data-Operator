3d300dfee56c46d9958b53bab4259af21fc6ce8c
Бекзадбек Насирахунов <n.bekzad@gmail.com>
2021-01-13 15:33:49 +0600

    Payment Method changes

--- sell/src/main/java/kg/nurtelecom/sell/ui/fragment/payment_method/PaymentByCardFragment.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/fragment/payment_method/PaymentByCardFragment.kt
@@ -1,8 +1,6 @@
 package kg.nurtelecom.sell.ui.fragment.payment_method
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import kg.nurtelecom.sell.core.CoreFragment
