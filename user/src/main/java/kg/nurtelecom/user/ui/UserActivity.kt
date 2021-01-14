441109afc27384994ed895e0fa6d6e8578f91102
akinahikaru <bakytova120496@gmail.com>
2021-01-14 21:08:31 +0600

    added custom view EditTextField + added custom method

--- user/src/main/java/kg/nurtelecom/user/ui/UserActivity.kt
+++ user/src/main/java/kg/nurtelecom/user/ui/UserActivity.kt
@@ -4,7 +4,9 @@ import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import kg.nurtelecom.core.activity.CoreActivity
 import kg.nurtelecom.core.extension.enable
+import kg.nurtelecom.core.extension.setToolbarTitle
 import kg.nurtelecom.core.extension.text
+import kg.nurtelecom.user.R
 import kg.nurtelecom.user.databinding.ActivityUserBinding
 import java.util.regex.Pattern
 
@@ -31,29 +33,34 @@ class UserActivity : CoreActivity<ActivityUserBinding, UserVM>(UserVM::class) {
     }
 
     private fun editTextHandler(): Array<String> {
-        val surname = text(vb.etSurname)
-        val name = text(vb.etName)
-        val middleName = text(vb.etMiddlename)
-        val phone = text(vb.etPhone)
-        val inn = text(vb.etIdentification)
+        val surname = vb.etSurname.getText()
+        val name = vb.etName.getText()
+        val middleName = vb.etMiddlename.getText()
+        val phone = vb.etPhone.getText()
+        val inn = vb.etIdentification.getText()
         return arrayOf(surname, name, middleName, phone, inn)
     }
 
     override fun setupViews() {
         super.setupViews()
         vm.fetchUserData()
-        title = "Профиль"
+        setSupportActionBar(vb.toolbar)
+        setToolbarTitle("Профиль")
+        vb.toolbar.setNavigationIcon(R.drawable.ic_baseline_close_24)
+        vb.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
-        vb.etSurname.addTextChangedListener {
+        vb.etSurname.setTextChangedListener {
             val (surname, name, phone) = editTextHandler()
             vb.btnChange.enable(surname.isNotEmpty() && name.isNotEmpty() && phone.isNotEmpty())
         }
 
-        vb.etName.addTextChangedListener {
+        vb.etName.setTextChangedListener {
             val (surname, name, phone) = editTextHandler()
             vb.btnChange.enable(surname.isNotEmpty() && name.isNotEmpty() && phone.isNotEmpty())
         }
-        vb.etPhone.addTextChangedListener {
+        vb.etPhone.setTextChangedListener {
             val (surname, name, phone) = editTextHandler()
             vb.btnChange.enable(surname.isNotEmpty() && name.isNotEmpty() && phone.isNotEmpty())
         }
@@ -63,7 +70,7 @@ class UserActivity : CoreActivity<ActivityUserBinding, UserVM>(UserVM::class) {
 
         }
 
-        vb.etIdentification.addTextChangedListener {
+        vb.etIdentification.setTextChangedListener {
             validateOfTheIdentificationNumber()
         }
     }
@@ -74,7 +81,7 @@ class UserActivity : CoreActivity<ActivityUserBinding, UserVM>(UserVM::class) {
 
         val ID_PATTERN =
             Pattern.compile("^(1|2)(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])(19|20)\\d\\d\\d\\d\\d\\d\\d\$")
-        val identificationNumberInput = vb.etIdentification.text.toString().trim()
+        val identificationNumberInput = vb.etIdentification.getText().trim()
         if (!ID_PATTERN.matcher(identificationNumberInput).matches()) {
             vb.etIdentification.error = "The ID number is not correct"
             return false
