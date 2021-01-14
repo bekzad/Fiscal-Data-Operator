45021f43a1b8f1730c2b201debbff01704d89379
Бектурсун Курманбеков <kurmanbekov46@gmail.com>
2021-01-14 12:35:17 +0600

    refactoring, task #7.01

--- sell/src/main/java/kg/nurtelecom/sell/ui/fragment/history/detail/HistoryDetailFragment.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/fragment/history/detail/HistoryDetailFragment.kt
@@ -1,33 +1,30 @@
 package kg.nurtelecom.sell.ui.fragment.history.detail
 
-import androidx.appcompat.app.AppCompatActivity
+import kg.nurtelecom.core.extension.parentActivity
 import kg.nurtelecom.core.extension.setToolbarTitle
 import kg.nurtelecom.core.fragment.CoreFragment
 import kg.nurtelecom.sell.R
 import kg.nurtelecom.sell.databinding.ChecksHistoryDetailFragmentBinding
-import kg.nurtelecom.sell.ui.fragment.history.HistoryAdapter
 import kg.nurtelecom.sell.ui.fragment.history.HistoryViewModel
 
 class HistoryDetailFragment : CoreFragment<ChecksHistoryDetailFragmentBinding, HistoryViewModel>(HistoryViewModel::class) {
 
     override fun onResume() {
         super.onResume()
-        (activity as AppCompatActivity).setToolbarTitle(R.string.history_title)
+        parentActivity.setToolbarTitle(R.string.history_title)
     }
 
-    override fun setupViews() {
-    }
+    override fun setupViews() {}
+
+    override fun getBinding() = ChecksHistoryDetailFragmentBinding.inflate(layoutInflater)
 
     override fun subscribeToLiveData() {
         super.subscribeToLiveData()
         observeCheckHistory()
     }
 
-    private fun observeCheckHistory() {
-
-    }
-
-    override fun getBinding() = ChecksHistoryDetailFragmentBinding.inflate(layoutInflater)
+    // TODO("change fun name to subscribe")
+    private fun observeCheckHistory() {}
 
     companion object {
         fun newInstance(): HistoryDetailFragment {
