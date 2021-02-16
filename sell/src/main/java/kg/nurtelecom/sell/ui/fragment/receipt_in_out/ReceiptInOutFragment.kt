bd69bcd31a91ff4a3f2a7f8263639bd141e0f283
Эрлан Алмаз <erlan.almaz10@gmail.com>
2021-02-16 15:15:36 +0600

    Setup buttons

--- sell/src/main/java/kg/nurtelecom/sell/ui/fragment/receipt_in_out/ReceiptInOutFragment.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/fragment/receipt_in_out/ReceiptInOutFragment.kt
@@ -3,11 +3,14 @@ package kg.nurtelecom.sell.ui.fragment.receipt_in_out
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import kg.nurtelecom.core.extension.parentActivity
 import kg.nurtelecom.core.extension.replaceFragment
+import kg.nurtelecom.data.receipt_in_out.ReceiptInOutType
 import kg.nurtelecom.sell.R
 import kg.nurtelecom.sell.core.CoreFragment
 import kg.nurtelecom.sell.databinding.FragmentReceiptInOutBinding
+import java.math.BigDecimal
 
 class ReceiptInOutFragment : CoreFragment<FragmentReceiptInOutBinding, ReceiptInOutVM>(ReceiptInOutVM::class) {
 
@@ -25,6 +28,44 @@ class ReceiptInOutFragment : CoreFragment<FragmentReceiptInOutBinding, ReceiptIn
         vb.btnConfirm.setOnClickListener {
             vm.generateReceiptInOut()
         }
+
+        vb.btnReceiptIn.setOnClickListener {
+            setupIncome()
+        }
+
+        vb.btnReceiptOut.setOnClickListener {
+            setupOutcome()
+        }
+
+        vb.icInputSum.fetchTextState {
+            if (!it.isNullOrEmpty()) {
+                vm.sum = BigDecimal(it.toString())
+            }
+        }
+    }
+
+    private fun setupIncome() {
+        vb.btnReceiptIn.apply {
+            setTextColor(resources.getColor(R.color.white))
+            backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.green)
+        }
+        vb.btnReceiptOut.apply {
+            setTextColor(resources.getColor(R.color.black))
+            backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.white)
+        }
+        vm.receiptInOutType = ReceiptInOutType.INCOME
+    }
+
+    private fun setupOutcome() {
+        vb.btnReceiptOut.apply {
+            setTextColor(resources.getColor(R.color.white))
+            backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.green)
+        }
+        vb.btnReceiptIn.apply {
+            setTextColor(resources.getColor(R.color.black))
+            backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.white)
+        }
+        vm.receiptInOutType = ReceiptInOutType.OUTCOME
     }
 
     companion object {
--- sell/src/main/java/kg/nurtelecom/sell/ui/fragment/receipt_in_out/ReceiptInOutVM.kt
+++ sell/src/main/java/kg/nurtelecom/sell/ui/fragment/receipt_in_out/ReceiptInOutVM.kt
@@ -2,20 +2,36 @@ package kg.nurtelecom.sell.ui.fragment.receipt_in_out
 
 import android.util.Log
 import kg.nurtelecom.core.viewmodel.CoreViewModel
+import kg.nurtelecom.data.receipt_in_out.ReceiptInOutRequest
+import kg.nurtelecom.data.receipt_in_out.ReceiptInOutType
 import kg.nurtelecom.sell.repository.ReceiptInOutRepository
 import kotlinx.coroutines.Dispatchers
+import java.math.BigDecimal
 
 
 abstract class ReceiptInOutVM : CoreViewModel() {
     abstract fun generateReceiptInOut()
+
+    abstract var receiptInOutType: ReceiptInOutType
+    abstract var sum: BigDecimal
 }
 
 class ReceiptInOutVMImpl(private val receiptInOutRepository: ReceiptInOutRepository) : ReceiptInOutVM() {
 
+    override var receiptInOutType: ReceiptInOutType = ReceiptInOutType.INCOME
+    override var sum: BigDecimal = BigDecimal.ZERO
+
+
     override fun generateReceiptInOut() {
+
+        val request = ReceiptInOutRequest(null, receiptInOutType, sum)
+        Log.i("ERLAN", request.toString())
+
         safeCall(Dispatchers.Default) {
             Log.i("ERLAN", receiptInOutRepository.generateReceiptInOut().toString())
         }
     }
+
+
 
 }
