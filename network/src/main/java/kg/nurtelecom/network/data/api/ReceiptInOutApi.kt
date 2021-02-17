3f56dda410e04030263ddef908c48fc4423e5aa2
Эрлан Алмаз <erlan.almaz10@gmail.com>
2021-02-17 12:33:45 +0600

    add request to api

--- network/src/main/java/kg/nurtelecom/network/data/api/ReceiptInOutApi.kt
+++ network/src/main/java/kg/nurtelecom/network/data/api/ReceiptInOutApi.kt
@@ -1,5 +1,6 @@
 package kg.nurtelecom.network.data.api
 
+import kg.nurtelecom.data.receipt_in_out.ReceiptInOutHistoryRequest
 import kg.nurtelecom.data.receipt_in_out.ReceiptInOutRequest
 import kg.nurtelecom.data.receipt_in_out.ReceiptInOutRequestResult
 import retrofit2.http.Body
@@ -12,5 +13,11 @@ interface ReceiptInOutApi {
     suspend fun generateReceiptInOut(
             @Header("Authorization") accessToken: String,
             @Body receiptInOutRequest: String
+    ): String
+
+    @POST("receiptinout/history")
+    suspend fun fetchHistoryReceiptInOutList(
+        @Header("Authorization") accessToken: String,
+        @Body receiptInOutHistoryRequest: String
     ): String
 }
